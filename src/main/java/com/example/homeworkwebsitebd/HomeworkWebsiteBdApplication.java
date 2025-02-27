c9b5473d378920ca4013d94cd0ea60aac9cd69a8
dianazavr1 <2235.10002@manas.edu.kg>
2025-02-27 21:57:18 +0600

    first commit

---
+++ src/main/java/com/example/homeworkwebsitebd/HomeworkWebsiteBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkwebsitebd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkWebsiteBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkWebsiteBdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkwebsitebd/HomeworkWebsiteBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkwebsitebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkWebsiteBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
